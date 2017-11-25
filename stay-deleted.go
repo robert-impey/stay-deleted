f67664bbb7c18929b8821a2867f7a67d2d164e5e
Robert Impey <robert@impey.info>
2017-11-25 21:55:44 +0000

    Able to delete marked files.
    However, deleting files during the walk causes an error.
    
    This needs to be tested with deleting more than one file
    in a directory.
    
    It could be rewritten to build up a list of files and directories
    to be deleted at the end of the walk.

--- stay-deleted.go
+++ stay-deleted.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/md5"
 	"flag"
 	"fmt"
@@ -21,31 +22,90 @@ func main() {
 	flag.Parse()
 
 	if directoryToSweep != "" {
-		SweepDirectory(directoryToSweep)
+		sweepDirectory(directoryToSweep)
 	} else if fileToMark != "" {
-		MarkFile(fileToMark)
+		markFile(fileToMark)
 	} else if fileToUnmark != "" {
-		UnmarkFile(fileToUnmark)
+		unmarkFile(fileToUnmark)
 	} else {
 		fmt.Println("Please tell me what to do!")
 	}
 }
 
-func SweepDirectory(directoryToSweep string) {
+func sweepDirectory(directoryToSweep string) {
 	var absDirectoryToSweep, err = filepath.Abs(directoryToSweep)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to find the absolute path for '%v'!\n",
 			directoryToSweep)
 	} else {
 		fmt.Printf("Sweeping: '%v'!\n", absDirectoryToSweep)
+		err := filepath.Walk(absDirectoryToSweep, walker)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
 	}
 }
 
-func MarkFile(fileToMark string) {
+func walker(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return err
+	} else {
+		if info.IsDir() && info.Name() == ".stay-deleted" {
+			sdFolder := path
+			fmt.Printf("Search SD folder '%v'\n", sdFolder)
+			containingFolder := filepath.Dir(sdFolder)
+			fmt.Printf("Containing folder '%v'\n", containingFolder)
+
+			sdFiles, err := filepath.Glob(filepath.Join(sdFolder, "*.txt"))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+			} else {
+				for _, sdFile := range sdFiles {
+					fmt.Printf("SD File '%v'\n", sdFile)
+					err := processSdFile(sdFile, containingFolder)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "%v\n", err)
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func processSdFile(sdFileName string, containingFolder string) error {
+	sdFile, err := os.Open(sdFileName)
+	defer sdFile.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return err
+	}
+
+	input := bufio.NewScanner(sdFile)
+	input.Scan()
+	fileToProcessName := filepath.Join(containingFolder, input.Text())
+	input.Scan()
+	action := input.Text()
+
+	if action == "delete" {
+		fmt.Printf("Deleting '%v'\n", fileToProcessName)
+		os.RemoveAll(fileToProcessName)
+	} else if action == "keep" {
+		fmt.Printf("Keeping '%v'\n", fileToProcessName)
+	} else {
+		return fmt.Errorf("Unrecognised action '%v'!\n", action)
+	}
+
+	return nil
+}
+
+func markFile(fileToMark string) {
 	setActionForFile(fileToMark, "delete")
 }
 
-func UnmarkFile(fileToUnmark string) {
+func unmarkFile(fileToUnmark string) {
 	setActionForFile(fileToUnmark, "keep")
 }
 
